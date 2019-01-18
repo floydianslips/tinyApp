556462b5bbaa31b565208d6547cf83f9e435ff29
joshua <josh@joshuadennis.ca>
2019-01-18 05:29:24 +0000

    many bug fixes, user restraint for deleting

--- express_server.js
+++ express_server.js
@@ -9,8 +9,8 @@ let templateVars = {
 };
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {url: "http://www.lighthouselabs.ca", userID: "dan" },
+  "9sm5xK": {url: "http://www.google.com", userID: "tim" }
 };
 
 const users = { 
@@ -19,12 +19,12 @@ const users = {
     email: "tim@tim.com", 
     password: "tim"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
+ "dan": {
+    id: "dan", 
+    email: "dan@dan.com", 
+    password: "dan"
   }
-}
+};
 
 //create random 6 charactor string for shortUrl key
 const possibleAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -65,28 +65,41 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   let templateVars = { user: users[req.cookies.user_id] };
+  let user;
   for(var value in users) {
-    if (users[value].email === req.body.email && users[value].password === req.body.password) {
-      
-      let id = generateRandomString(9, possibleNum);
-      users[id] = { id: id, email: req.body.email, password: req.body.password };
-      res.cookie("user_id", id);
+    if (users[value].email === req.body.email) {
+      if (users[value].password === req.body.password) {
+        user = users[value];
+      }
+    }    
+  }
+    
+    if (user) {
+      // users[value].password === req.body.password) {
+      console.log('help')
+      // let id = generateRandomString(9, possibleNum);
+      // users[id] = { id: id, email: req.body.email, password: req.body.password };
+      res.cookie("user_id", user.id);
+      // console.log(users[value].id)
       res.redirect("/urls");
       return;
-    } else { res.sendStatus(403);
+    } else { res.render("urls_login", templateVars);
     return;
     }
-  }
+  
 });
 
 app.post("/register", (req, res) => {
   let templateVars = { user: users[req.cookies.user_id] };
+  console.log(req.body)
   for(var value in users) {
       if (users[value].email !== req.body.email && req.body.email && req.body.password) {
         let id = generateRandomString(9, possibleNum);
-        users[id] = { id: id, email: req.body.email, pasnewsword: req.body.password };
+        console.log(id);
+        users[id] = { id: id, email: req.body.email, password: req.body.password };
         res.cookie("user_id", id);
         res.redirect("/urls");
+        console.log(users)
       
         return;
       } else { res.status(400);
@@ -105,24 +118,19 @@ app.get('/urls', (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-  // let templateVars = { user: users[req.cookies.user_id] };
-  // if (req.cookies.user_id) {
-    // delete urlDatabase[req.params.id];
-    // res.redirect(301, "/urls");
-    // } else { res.render("urls_register", templateVars); }
   });
 
 
 
 app.post("/urls", (req, res) => {
   let templateVars = { user: users[req.cookies.user_id] };
-  console.log(req.cookies)
   if (req.cookies.user_id) {
-    console.log(req.body.longURL)
     let randomKey = generateRandomString(6, possibleAll);
-    urlDatabase[randomKey] = { url: "http://" + req.body.longURL, userID: req.cookies.user_id }
-    console.log(urlDatabase);
+    console.log(req.body.longURL)
+    urlDatabase[randomKey] = { url: "http://" + req.body.longURL, userID: req.cookies.user_id };
+    console.log(urlDatabase)
     res.redirect(`/urls/${randomKey}`);
+
   } else { 
     res.redirect(301, "/urls"); 
   }
@@ -131,10 +139,11 @@ app.post("/urls", (req, res) => {
 
 app.post('/urls/:id/delete', (req, res) => {
   let templateVars = { user: users[req.cookies.user_id] }; 
-  if (req.cookies.user_id) {
+  console.log(req.cookies.user_id);
+  if (req.cookies.user_id === urlDatabase[req.params.id].userID) {
   delete urlDatabase[req.params.id];
   res.redirect(301, "/urls");
-  } else { res.redirect(301, "/register"); }
+  } else { res.redirect(301, "/urls"); }
 });
 
 app.post("/urls/:id/update", (req, res) => {
