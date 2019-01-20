d3d23f0978503085d7c5abc3cd67d624e81341d9
joshua <josh@joshuadennis.ca>
2019-01-20 00:10:48 +0000

    finished with bugs...hopefully

--- express_server.js
+++ express_server.js
@@ -3,18 +3,19 @@ const express = require("express");
 // const cookieParser =require("cookie-parser");
 const cookieSession = require('cookie-session');
 const app = express();
+const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
+const PORT = 8080; // default port 8080
+const possibleAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const possibleNum = "1234567890";
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ["key1", "key2"],
-  // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
-const bcrypt = require('bcrypt');
-const PORT = 8080; // default port 8080
-app.set('view engine', 'ejs');
-// app.use(cookieParser());
-// let templateVars = {
-// };
 
 const urlDatabase = {
   "b2xVn2": {url: "http://www.lighthouselabs.ca", userID: "dan" },
@@ -24,66 +25,17 @@ const urlDatabase = {
 };
 
 const users = { 
-  
-  
   "368466822": { 
     id: "368466822",
     email: 'nomore2pick@gmail.com',
     password: '$2b$10$NgKYiA7h5iC/VKotyDpXK.voW/4pg9A2YT.fJNlys80txvZ083mDm' }
-  
 };
-
-//create random 6 charactor string for shortUrl key
-const possibleAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-const possibleNum = "1234567890";
-
-function generateRandomString(num, type) { //borrowed from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-  var text = "";
-  
-  for (var i = 0; i < num; i++)
-    text += type.charAt(Math.floor(Math.random() * type.length));
-  return text;
-}
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post("/logout", (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  req.session = null; 
-  res.render("urls_login", templateVars);
-
-});
-
-app.get("/urls/new", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.session.user_id] }
-   // let templateVars = { user: users[req.session.user_id] };
-   // if (!req.session.user_id) {  
-   res.render("urls_new", templateVars);
-   // } else { res.render("urls_login", templateVars); }
- });
-
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.session.user_id] };
-  console.log(typeof req.params.id);
-  if (req.params.id === "new") {
-    console.log("help");
-    res.redirect(303, "/urls/new");
-    return;
-   }
-  else if (!urlDatabase[req.params.id]) {
-    res.send("Sorry that shortened URL doesn't exist within the bounds of reality");
-  } else if (req.session.user_id === urlDatabase[req.params.id].userID) {
-    res.render("urls_show", templateVars);
-  } else if (req.session.user_id && req.session.user_id) {
-    res.send("<h2>You do not own this so you can modify it:</h2><a href=/urls>Back to your URLs</a>");
-  }
-  else { res.render("urls_login", templateVars); }
-});
 
 app.get("/login", (req, res) => {
   let templateVars = { user: users[req.session.user_id] };
-  res.render("urls_login");
+  if (req.session.user_id) {
+    res.redirect(303, "/urls");
+  } else { res.render("urls_login"); }
 });
 
 app.post("/login", (req, res) => {
@@ -100,14 +52,59 @@ app.post("/login", (req, res) => {
       req.session.user_id = user.id;
       res.redirect("/urls");
       return;
-    } else { res.render("urls_login", templateVars);
+    } else { res.send("<h2>Something is amiss, please try again.</h2><a href=/login>Login</a>");
     return;
     }
 });
 
+app.post("/logout", (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  req.session = null; 
+  res.render("urls_login", templateVars);
+
+});
+
+app.get("/", (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  if (req.session.user_id) {
+    res.redirect(301, "/urls");
+    } else { res.redirect(301, "/login"); }
+  res.redirect(302, "/urls");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { userUrls: getUrlsForUserById(req.session.user_id), user: users[req.session.user_id] };
+  if (req.session.user_id) {
+  res.render('urls_index', templateVars);
+  } else { res.send("<h2>You are not logged in.</h2><a href=/login>Login</a>"); }
+});
+
+app.post("/urls", (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  if (req.session.user_id) {
+    let randomKey = generateRandomString(6, possibleAll);
+    urlDatabase[randomKey] = { url: "http://" + req.body.longURL, userID: req.session.user_id };
+    res.redirect(`/urls/${randomKey}`);
+
+  } else { 
+    res.render("urls_login", templateVars); 
+  }
+
+});
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.session.user_id] }
+   // let templateVars = { user: users[req.session.user_id] };
+   if (req.session.user_id) {  
+   res.render("urls_new", templateVars);
+   } else { res.render("urls_login", templateVars); }
+ });
+
 app.get("/register", (req, res) => {
   let templateVars = { user: users[req.session.user_id] };
-  res.render('urls_register', templateVars);
+  if (req.session.user_id) {
+    res.redirect(303, "/urls");
+  } else { res.render("urls_register", templateVars) };
 });
 
 app.post("/register", (req, res) => {
@@ -121,34 +118,16 @@ app.post("/register", (req, res) => {
         users[id] = { id: id, email: req.body.email, password: hashedPassword };
         req.session.user_id = users[id].id;
         res.redirect(301, "/urls");
+        console.log(hashedPassword)
         return;
-      } else { res.status(400);
-         res.render("urls_register", templateVars);
+      } else if (users[value].email === req.body.email) {
+          res.send("<h2>It appears you already have an account. Please log in.</h2><a href=/login>Login</a>");          
+      }
+      else {
+         res.send("<h2>You need to enter a valid email and a password.</h2><a href=/register>Try to register again</a>");
       return;
         }
     }
-});
-
-app.get('/urls', (req, res) => {
-  let templateVars = { userUrls: getUrlsForUserById(req.session.user_id), user: users[req.session.user_id] };
-  // if (req.session.user_id) {
-  res.render('urls_index', templateVars);
-  // } else { res.send("<h2>You are not logged in.</h2><a href=/login>Login</a>"); }
-});
-
-
-
-app.post("/urls", (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  if (req.session.user_id) {
-    let randomKey = generateRandomString(6, possibleAll);
-    urlDatabase[randomKey] = { url: "http://" + req.body.longURL, userID: req.session.user_id };
-    res.redirect(`/urls/${randomKey}`);
-
-  } else { 
-    res.render("urls_login", templateVars); 
-  }
-
 });
 
 app.post('/urls/:id/delete', (req, res) => {
@@ -176,12 +155,20 @@ app.get("/u/:shortURL", (req, res) => {
   } else { res.redirect(301, urlDatabase[req.params.shortURL].url); }
 });
 
-app.get("/", (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  if (req.session.user_id) {
-    res.redirect(301, "/urls");
-    } else { res.redirect(301, "/login"); }
-  res.redirect(302, "/urls");
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.session.user_id] };
+  if (req.params.id === "new") {
+    res.redirect(303, "/urls/new");
+    return;
+   }
+  else if (!urlDatabase[req.params.id]) {
+    res.send("Sorry that shortened URL doesn't exist within the bounds of reality");
+  } else if (req.session.user_id === urlDatabase[req.params.id].userID) {
+    res.render("urls_show", templateVars);
+  } else if (req.session.user_id && req.session.user_id) {
+    res.send("<h2>You do not own this so you can modify it:</h2><a href=/urls>Back to your URLs</a>");
+  }
+  else { res.render("urls_login", templateVars); }
 });
 
 app.get("/urls.json", (req, res) => {
@@ -198,6 +185,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// generate object of all the short urls for logged in user
 function getUrlsForUserById (user) {
   let usersShorUrls = {};
   for (let shortUrl in urlDatabase) {
@@ -208,4 +196,12 @@ function getUrlsForUserById (user) {
   return usersShorUrls;
 }
 
+//create random 6 charactor string for shortUrl key
+function generateRandomString(num, type) { //borrowed from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  var text = "";
+  
+  for (var i = 0; i < num; i++)
+    text += type.charAt(Math.floor(Math.random() * type.length));
+  return text;
+}
 // make this geturls to work with /urls
